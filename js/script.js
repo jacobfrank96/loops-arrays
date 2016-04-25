bf2873918fe09f801917fb5c7e39523d5deb9811
simonyost <simonyost@gmail.com>
2016-04-25 18:11:08 -0500

    demo

--- js/script.js
+++ js/script.js
@@ -1,12 +1,33 @@
 // loop
-var count = 0;
+var count = 1;
 var list = '';
 
-while (count < 10) {
+while (count < 11) {
   list += '<li>current count is: ' + count + '</li>';
   count += 1;
 }
 
 // output results
 document.write("<h1>Loops</h1>");
-document.write("<ul>" + list + "</ul>");
+document.write("<ol>" + list + "</ol>");
+
+
+
+// array
+
+var myItems = [
+  ["question one", "answer one"],
+  ["question two", "answer two"]
+];
+
+var secondList ="<ul>";
+var i = 0;
+
+while (i < myItems.length) {
+    secondList += "<li>" + myItems[i] + "</li>";
+    i++;
+}
+
+secondList += "</ul>";
+
+document.write(secondList);
